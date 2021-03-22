d7c3c153a80013858881886e3851542f0b678e88
Tom Léost <hello@urbentom.io>
2021-03-22 21:33:14 +0000

    Add base HomeScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={HomeScreen} />
+				</Switch>
+			</Router>
+		</Wrapper>
+	);
 }
 
 export default App;
+
+const Wrapper = styled.div`
+	height: 100vh;
+	width: 100vw;
+	background-color: #242526;
+`;
